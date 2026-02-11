35d2df6d2d15de8c4a0eaf49e401898a08387212
zera-nameless-nn <hau0933223733@gmail.com>
2026-02-11 19:52:26 +0700

    Delete App.tsx

--- App.tsx
+++
@@ -1,5 +0,0 @@
-// This file is deprecated. Logic has been moved to Main.tsx.
-// Please verify index.tsx imports Main from './Main'.
-import React from 'react';
-const App = () => <div>Deprecated</div>;
-export default App;
